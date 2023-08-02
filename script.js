3ba6ea50532db8dc86ad00733e81e88cdec2879c
Fl1pMoura <130615128+Fl1pMoura@users.noreply.github.com>
2023-08-02 11:11:50 -0300

    copy to clipboard no footer e mais umas firula

--- script.js
+++ script.js
@@ -127,6 +127,26 @@ function scrollToTarget(event) {
   });
 
 
+  const address = document.getElementById(`address`)
+  const phone = document.getElementById(`phone`)
+  const mail = document.getElementById(`mail`)
+  const copyText = document.getElementById(`copied`)
+  console.log(copyText)
 
+  address.addEventListener('click', copyToClipboard)
+  phone.addEventListener('click', copyToClipboard)
+  mail.addEventListener('click', copyToClipboard)
+
+  function copyToClipboard(ev) {
+    navigator.clipboard.writeText(ev.currentTarget.innerText);
+  
+    // Adiciona a classe "visible" para fazer o elemento aparecer
+    copyText.classList.add('visible');
+  
+    // Aguarda 2 segundos (2000 milissegundos) e, em seguida, remove a classe "visible" para fazer o elemento sumir
+    setTimeout(function () {
+      copyText.classList.remove('visible');
+    }, 1500);
+  }
 
 
